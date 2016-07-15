a0e39ae0e7a6d85e4260268c2940a58edd9d719e
Ankush Khanna <a.khanna@rebuy.de>
2016-07-15 16:13:49 +0200

    Adding comments.

--- src/main/scala/actor/fsm/AggregateMessages.scala
+++ src/main/scala/actor/fsm/AggregateMessages.scala
@@ -13,7 +13,7 @@ case object Collect extends State
 
 case object Caller extends State
 
-case class Message[T](message: T)
+case class Message[T](value: T)
 
 case class Apply()
 
@@ -21,6 +21,19 @@ sealed case class Data[T](queue: Q[Message[T]])
 
 case class Rate(noOfMessage: Int, time: FiniteDuration)
 
+/**
+ * Aggregate Message, has 3 stages:
+ * Ideal: does nothing
+ * Collect: excepts Message and store it in a queue.
+ * Caller: calls the function passed as parameter, with the collected message.
+ *
+ * Transitions:
+ * Ideal -> Collect : When a message is received.
+ * Collect -> Caller : When either the time specified in Rate is expired
+ * or the number of message collected are greater than noOfMessages specified in Rate.
+ * Caller -> Ideal : After calling the function.
+ *
+ * */
 class AggregateMessages[T](f: List[T] => Unit, rate: Rate) extends Actor with FSM[State, Data[T]] {
 
   startWith(Idle, Data(Q[Message[T]]()))
@@ -44,7 +57,7 @@ class AggregateMessages[T](f: List[T] => Unit, rate: Rate) extends Actor with FS
 
   when(Caller) {
     case Event(a: Apply, d: Data[T]) =>
-      f.apply(d.queue.take(rate.noOfMessage).map(_.message).toList)
+      f.apply(d.queue.take(rate.noOfMessage).map(_.value).toList)
       goto(Idle) using d.copy(queue = d.queue.drop(rate.noOfMessage))
     case Event(m: Message[T], d: Data[T]) =>
       stay() using d.copy(queue = d.queue.enqueue(m))
